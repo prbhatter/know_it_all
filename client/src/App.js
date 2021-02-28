817c89a9d2846b51107bc4ec756279432344d5ea
Pradyumn Bhatter <55000494+prbhatter@users.noreply.github.com>
2021-02-28 17:20:58 +0530

    SignIn and SignUp forms frontend

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
 import Dashboard from './components/dashboard/Dashboard'
 import QuestionDetails from './components/questions/QuestionDetails'
+import SignIn from './components/auth/SignIn'
+import SignUp from './components/auth/SignUp'
 
 function App() {
   return (
@@ -9,8 +11,10 @@ function App() {
       <div className="App">
         <Navbar />
         <Switch>
-            <Route exact path='/'component={Dashboard} />
-            <Route path='/question/:id'component={QuestionDetails} />
+            <Route exact path='/' component={Dashboard} />
+            <Route path='/question/:id' component={QuestionDetails} />
+            <Route path='/signin' component={SignIn} />
+            <Route path='/signup' component={SignUp} />
         </Switch>
       </div>
     </BrowserRouter>
