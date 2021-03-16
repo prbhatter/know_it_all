f4df77a38c8d34828e8dbd3a24b53fbed4f09d58
Ansh <sunilpasari406@gmail.com>
2021-03-17 00:05:48 +0530

    minor change

--- client/src/App.js
+++ client/src/App.js
@@ -13,15 +13,7 @@ class App extends Component {
   // const { questions } = this.props
   
   render() {
-    // console.log(this.props.isAuthenticated)
-    // const redir = this.props.isAuthenticated == true && <Redirect to='/' />
-    // const SignInComponent = redir && <SignIn />
-    // const SignUpComponent = redir && <SignUp />
-
-    // const redir2 = this.props.isAuthenticated == false && <Redirect to='/' />
-    // const RaiseQuestionComponent = redir2 && <RaiseQuestion />
-    // const MyQuestionsComponent = redir2 && <MyQuestions />
-    return (
+   return (
       <BrowserRouter>
         <div className="App">
           <Navbar />
