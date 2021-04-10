827ae3304b7cebbcbaea89e4b612ac421dabf35d
Ansh <sunilpasari406@gmail.com>
2021-04-10 11:20:02 +0530

    Assigned Questions feature added for tutors

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -17,7 +17,7 @@ class Dashboard extends Component {
       <div className="dashboard container">
         <div className="row">
           <div className="col s12 m6">
-            <QuestionList questions={questions}/>
+            <QuestionList questions={questions}/> 
           </div>
           <div className="col s12 m5 offset-m1">
             <Notifications />
